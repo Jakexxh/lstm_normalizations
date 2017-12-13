fcf440f972e2f4daed3602c2351494ff68b558e4
jake_ict <xuexiaohe@ict.ac.cn>
2017-12-13 10:30:53 +0800

    nothing

--- src/ptb/run.sh
+++ src/ptb/run.sh
@@ -47,23 +47,23 @@
 #python mnist/test_784*1.py --cell=bn_sep --log_dir=/tmp/logs/mnist_iden_tr/bn_sep_0.01_0.1 --lr=0.1
 #python mnist/test_784*1.py --cell=bn_sep --log_dir=/tmp/logs/mnist_iden_tr/bn_sep_0.01_1.0 --lr=1.0
 
-python ptb_word_lm.py --lr=1.0 --rnn_mode=basic --num_gpus=1 --save_path=/tmp/log/ptb_ref_base/basic_1.0
+python ptb_word_lm.py --lr=1.0 --rnn_mode=base --num_gpus=0 --save_path=/tmp/log/ptb_ref_base/basic_1.0
 
-python ptb_word_lm.py --lr=1.0 --rnn_mode=cn_sep --num_gpus=1 --save_path=/tmp/log/ptb_ref_base/cn_sep_g1.0_1.0
+python ptb_word_lm.py --lr=1.0 --rnn_mode=cn_sep --num_gpus=0 --save_path=/tmp/log/ptb_ref_base/cn_sep_g1.0_1.0
 
-python ptb_word_lm.py --lr=1.0 --rnn_mode=wn_sep --num_gpus=1 --save_path=/tmp/log/ptb_ref_base/wn_sep_g1.0_1.0
+python ptb_word_lm.py --lr=1.0 --rnn_mode=wn_sep --num_gpus=0 --save_path=/tmp/log/ptb_ref_base/wn_sep_g1.0_1.0
 
-python ptb_word_lm.py --lr=1.0 --rnn_mode=ln_sep --num_gpus=1 --save_path=/tmp/log/ptb_ref_base/ln_sep_g0.1_1.0
+python ptb_word_lm.py --lr=1.0 --rnn_mode=ln_sep --num_gpus=0 --save_path=/tmp/log/ptb_ref_base/ln_sep_g0.1_1.0
 
-python ptb_word_lm.py --lr=1.0 --rnn_mode=bn_sep --num_gpus=1 --save_path=/tmp/log/ptb_ref_base/bn_sep_g0.1_1.0
+python ptb_word_lm.py --lr=1.0 --rnn_mode=bn_sep --num_gpus=0 --save_path=/tmp/log/ptb_ref_base/bn_sep_g0.1_1.0
 
-python ptb_word_lm.py --lr=1.0 --rnn_mode=pcc_sep --num_gpus=1 --save_path=/tmp/log/ptb_ref_base/pcc_sep_g1.0_1.0
+python ptb_word_lm.py --lr=1.0 --rnn_mode=pcc_sep --num_gpus=0 --save_path=/tmp/log/ptb_ref_base/pcc_sep_g1.0_1.0
 
 
-python ptb_word_lm_base.py --lr=1.0 --rnn_mode=cn_sep --num_gpus=1 --save_path=/tmp/log/ptb_ref_orig/cn_sep_g1.0_1.0
+python ptb_word_lm_base.py --lr=1.0 --rnn_mode=cn_sep --num_gpus=0 --save_path=/tmp/log/ptb_ref_orig/cn_sep_g1.0_1.0
 
-python ptb_word_lm_base.py --lr=1.0 --rnn_mode=wn_sep --num_gpus=1 --save_path=/tmp/log/ptb_ref_orig/wn_sep_g1.0_1.0
+python ptb_word_lm_base.py --lr=1.0 --rnn_mode=wn_sep --num_gpus=0 --save_path=/tmp/log/ptb_ref_orig/wn_sep_g1.0_1.0
 
-python ptb_word_lm_base.py --lr=1.0 --rnn_mode=ln_sep --num_gpus=1 --save_path=/tmp/log/ptb_ref_orig/ln_sep_g0.1_1.0
+python ptb_word_lm_base.py --lr=1.0 --rnn_mode=ln_sep --num_gpus=0 --save_path=/tmp/log/ptb_ref_orig/ln_sep_g0.1_1.0
 
-python ptb_word_lm_base.py --lr=1.0 --rnn_mode=pcc_sep --num_gpus=1 --save_path=/tmp/log/ptb_ref_orig/pcc_sep_g1.0_1.0
+python ptb_word_lm_base.py --lr=1.0 --rnn_mode=pcc_sep --num_gpus=0 --save_path=/tmp/log/ptb_ref_orig/pcc_sep_g1.0_1.0
