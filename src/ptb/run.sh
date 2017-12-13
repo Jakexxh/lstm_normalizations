8dc9af2286b42c3cd6dc2461fe1cc148e28eafd9
jake_hw2_ict <jakewill@qq.com>
2017-12-13 11:39:38 +0800

    up

--- src/ptb/run.sh
+++ src/ptb/run.sh
@@ -47,7 +47,7 @@
 #python mnist/test_784*1.py --cell=bn_sep --log_dir=/tmp/logs/mnist_iden_tr/bn_sep_0.01_0.1 --lr=0.1
 #python mnist/test_784*1.py --cell=bn_sep --log_dir=/tmp/logs/mnist_iden_tr/bn_sep_0.01_1.0 --lr=1.0
 
-python ptb_word_lm.py --lr=1.0 --rnn_mode=basic --num_gpus=1 --save_path=/tmp/log/ptb_ref_base/basic_1.0
+python ptb_word_lm.py --lr=1.0 --rnn_mode=base --num_gpus=1 --save_path=/tmp/log/ptb_ref_base/basic_1.0
 
 python ptb_word_lm.py --lr=1.0 --rnn_mode=cn_sep --num_gpus=1 --save_path=/tmp/log/ptb_ref_base/cn_sep_g1.0_1.0
 
@@ -60,10 +60,10 @@ python ptb_word_lm.py --lr=1.0 --rnn_mode=bn_sep --num_gpus=1 --save_path=/tmp/l
 python ptb_word_lm.py --lr=1.0 --rnn_mode=pcc_sep --num_gpus=1 --save_path=/tmp/log/ptb_ref_base/pcc_sep_g1.0_1.0
 
 
-python ptb_word_lm_base.py --lr=1.0 --rnn_mode=cn_sep --num_gpus=1 --save_path=/tmp/log/ptb_ref_orig/cn_sep_g1.0_1.0
+python ptb_word_lm_base.py --lr=1.0 --rnn_mode=cn_sep --num_gpus=1 --save_path=/tmp/log/ptb_orig/cn_sep_g1.0_1.0
 
-python ptb_word_lm_base.py --lr=1.0 --rnn_mode=wn_sep --num_gpus=1 --save_path=/tmp/log/ptb_ref_orig/wn_sep_g1.0_1.0
+python ptb_word_lm_base.py --lr=1.0 --rnn_mode=wn_sep --num_gpus=1 --save_path=/tmp/log/ptb_orig/wn_sep_g1.0_1.0
 
-python ptb_word_lm_base.py --lr=1.0 --rnn_mode=ln_sep --num_gpus=1 --save_path=/tmp/log/ptb_ref_orig/ln_sep_g0.1_1.0
+python ptb_word_lm_base.py --lr=1.0 --rnn_mode=ln_sep --num_gpus=1 --save_path=/tmp/log/ptb_orig/ln_sep_g0.1_1.0
 
-python ptb_word_lm_base.py --lr=1.0 --rnn_mode=pcc_sep --num_gpus=1 --save_path=/tmp/log/ptb_ref_orig/pcc_sep_g1.0_1.0
+python ptb_word_lm_base.py --lr=1.0 --rnn_mode=pcc_sep --num_gpus=1 --save_path=/tmp/log/ptb_orig/pcc_sep_g1.0_1.0
