506d7d3d22797340a7251d58351b44376091bf57
Jake Xue <jakewill@qq.com>
2017-12-22 11:33:59 +0800

    prepare norm cn

--- src/ptb/run.sh
+++ src/ptb/run.sh
@@ -3,6 +3,10 @@
 #python ptb_word_lm_base.py --lr=1.0 --g=1.0--rnn_mode=scale_cn --num_gpus=1 --save_path=/tmp/log/ptb_base
 #python ptb_word_lm_base.py ---lr=1.0 --g=10.0 --rnn_mode=scale_cn --num_gpus=1 --save_path=/tmp/log/ptb_base
 
+python ptb_word_lm_base.py --lr=1.0 --g=0.1 --rnn_mode=norm_cn --num_gpus=1 --save_path=/tmp/log/ptb_base
+python ptb_word_lm_base.py --lr=1.0 --g=1.0--rnn_mode=norm_cn --num_gpus=1 --save_path=/tmp/log/ptb_base
+python ptb_word_lm_base.py ---lr=1.0 --g=10.0 --rnn_mode=norm_cn --num_gpus=1 --save_path=/tmp/log/ptb_base
+
 #python ptb_word_lm.py --lr=1.0 --g=0.1 --rnn_mode=cn_sep --num_gpus=1 --save_path=/tmp/log/ptb_ref_new
 #python ptb_word_lm.py --lr=1.0 --g=1.0 --rnn_mode=cn_sep --num_gpus=1 --save_path=/tmp/log/ptb_ref_new
 #python ptb_word_lm.py --lr=1.0 --g=10.0 --rnn_mode=cn_sep --num_gpus=1 --save_path=/tmp/log/ptb_ref_new
@@ -11,9 +15,9 @@
 #python ptb_word_lm.py --lr=1.0 --g=1.0 --rnn_mode=pcc_sep --num_gpus=1 --save_path=/tmp/log/ptb_ref_new
 #python ptb_word_lm.py --lr=1.0 --g=10.0 --rnn_mode=pcc_sep --num_gpus=1 --save_path=/tmp/log/ptb_ref_new
 
-python ptb_word_lm.py --lr=1.0 --g=0.01 --rnn_mode=ln_sep --num_gpus=1 --save_path=/tmp/log/ptb_ref_new
-python ptb_word_lm.py --lr=1.0 --g=0.1 --rnn_mode=ln_sep --num_gpus=1 --save_path=/tmp/log/ptb_ref_new
-python ptb_word_lm.py --lr=1.0 --g=1.0 --rnn_mode=ln_sep --num_gpus=1 --save_path=/tmp/log/ptb_ref_new
+#python ptb_word_lm.py --lr=1.0 --g=0.01 --rnn_mode=ln_sep --num_gpus=1 --save_path=/tmp/log/ptb_ref_new
+#python ptb_word_lm.py --lr=1.0 --g=0.1 --rnn_mode=ln_sep --num_gpus=1 --save_path=/tmp/log/ptb_ref_new
+#python ptb_word_lm.py --lr=1.0 --g=1.0 --rnn_mode=ln_sep --num_gpus=1 --save_path=/tmp/log/ptb_ref_new
 
 #python ptb_word_lm.py --lr=1.0 --g=0.01 --rnn_mode=wn_sep --num_gpus=1 --save_path=/tmp/log/ptb_ref_new
 #python ptb_word_lm.py --lr=1.0 --g=0.1 --rnn_mode=wn_sep --num_gpus=1 --save_path=/tmp/log/ptb_ref_new
