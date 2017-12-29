8d437b832e7ec619188577f31393d6a9778e5984
Jake Xue <jakewill@qq.com>
2017-12-29 16:39:00 +0800

    to run!!!!

--- src/nmt/run.sh
+++ src/nmt/run.sh
@@ -107,7 +107,7 @@
 #    --dropout=0.2 \
 #    --metrics=bleu
 python -m nmt.nmt \
-   --unit_type=cn_sep \
+   --unit_type=pcc_sep \
     --src=vi --tgt=en \
     --learning_rate=1.0 \
     --grain=5.0 \
@@ -125,7 +125,58 @@ python -m nmt.nmt \
 
 
 python -m nmt.nmt \
-   --unit_type=cn_sep \
+   --unit_type=pcc_sep \
+    --src=vi --tgt=en \
+    --learning_rate=1.0 \
+    --grain=10.0 \
+    --vocab_prefix=../../data/nmt_data/vocab  \
+    --train_prefix=../../data/nmt_data/train \
+    --dev_prefix=../../data/nmt_data/tst2012  \
+    --test_prefix=../../data/nmt_data/tst2013 \
+    --out_dir=/tmp/nmt_model/nmt_test_b \
+    --num_train_steps=12000 \
+    --steps_per_stats=100 \
+    --num_layers=2 \
+    --num_units=128 \
+    --dropout=0.2 \
+    --metrics=bleu
+
+python -m nmt.nmt \
+   --unit_type=wn_sep \
+    --src=vi --tgt=en \
+    --learning_rate=0.1 \
+    --grain=10.0 \
+    --vocab_prefix=../../data/nmt_data/vocab  \
+    --train_prefix=../../data/nmt_data/train \
+    --dev_prefix=../../data/nmt_data/tst2012  \
+    --test_prefix=../../data/nmt_data/tst2013 \
+    --out_dir=/tmp/nmt_model/nmt_test_b \
+    --num_train_steps=12000 \
+    --steps_per_stats=100 \
+    --num_layers=2 \
+    --num_units=128 \
+    --dropout=0.2 \
+    --metrics=bleu
+
+python -m nmt.nmt \
+   --unit_type=wn_sep \
+    --src=vi --tgt=en \
+    --learning_rate=0.5 \
+    --grain=10.0 \
+    --vocab_prefix=../../data/nmt_data/vocab  \
+    --train_prefix=../../data/nmt_data/train \
+    --dev_prefix=../../data/nmt_data/tst2012  \
+    --test_prefix=../../data/nmt_data/tst2013 \
+    --out_dir=/tmp/nmt_model/nmt_test_b \
+    --num_train_steps=12000 \
+    --steps_per_stats=100 \
+    --num_layers=2 \
+    --num_units=128 \
+    --dropout=0.2 \
+    --metrics=bleu
+
+python -m nmt.nmt \
+   --unit_type=wn_sep \
     --src=vi --tgt=en \
     --learning_rate=1.0 \
     --grain=10.0 \
