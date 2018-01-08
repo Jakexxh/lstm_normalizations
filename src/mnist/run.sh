5cf829b1f8de37d85eb3cde64320545a0f3ac8ef
jake_hw2_ict <jakewill@qq.com>
2018-01-08 16:19:33 +0800

    up

--- src/mnist/run.sh
+++ src/mnist/run.sh
@@ -15,7 +15,7 @@
 #python test_784*1_base.py --cell=wn_sep --log_dir=/tmp/logs/mnist_final --g=0.1 --lr=0.01
 #python test_784*1_base.py --cell=wn_sep --log_dir=/tmp/logs/mnist_final --g=0.1 --lr=0.1
 #
-#python test_784*1_base.py --cell=base --log_dir=/tmp/logs/mnist_final --g=0.0 --lr=0.001
+python test_784*1_base.py --cell=base --log_dir=/tmp/logs/mnist_final --g=0.0 --lr=0.001
 python test_784*1_base.py --cell=base --log_dir=/tmp/logs/mnist_final --g=0.0 --lr=0.01
 python test_784*1_base.py --cell=base --log_dir=/tmp/logs/mnist_final --g=0.0 --lr=0.1
 
