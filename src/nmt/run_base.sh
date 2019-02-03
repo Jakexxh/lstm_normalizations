0c7e994591dce3684b211c7194152e9435200959
Jake Xue <jakewill@qq.com>
2019-02-03 15:43:33 +0800

    clean

--- src/nmt/run_base.sh
+++ src/nmt/run_base.sh
@@ -1,22 +1,5 @@
 #!/usr/bin/env bash
 
-## scale_cn for ptb_base
-#python -m nmt.nmt \
-#    --unit_type=layer_norm_lstm \
-#    --src=vi --tgt=en \
-#    --learning_rate=1.0 \
-#    --grain=0.0 \
-#    --vocab_prefix=../../data/nmt_data/vocab  \
-#    --train_prefix=../../data/nmt_data/train \
-#    --dev_prefix=../../data/nmt_data/tst2012  \
-#    --test_prefix=../../data/nmt_data/tst2013 \
-#    --out_dir=/tmp/nmt_model/default_layernorm \
-#    --num_train_steps=12000 \
-#    --steps_per_stats=100 \
-#    --num_layers=2 \
-#    --num_units=128 \
-#    --dropout=0.2 \
-#    --metrics=bleu
 
 export CUDA_VISIBLE_DEVICES=0
 python -m nmt.nmt \
